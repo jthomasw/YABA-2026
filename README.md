d5c6393fb370b27755233f9aa754bd2a0a742ce5
Kushith Konidela <kushithreddy8990@gmail.com>
2026-02-17 20:03:58 -0600

    updated HTML file and the go file

--- README.md
+++ README.md
@@ -5,3 +5,21 @@ Group 4 Yaba
 
 FLOWCHART
 ![Personal Income Spending Flowchart - United States - Imgur](https://github.com/user-attachments/assets/b099448e-1424-41c5-8988-a23a59e70d5f)
+
+You have to install the Golang into your computer. By using the given below link you can install it.
+https://go.dev/dl/ 
+
+After completion of the Golang Download
+
+Open terminal / command prompt
+Run:
+
+go version --> to get to know which version you are using.
+
+then run the command:
+go run main.go
+
+as you have a folder and it contains a main.go file in it then go to the new tab in chrome or edge then run:
+
+http://localhost:8080/templates/main.html
+
--- main.go
+++ main.go
@@ -2,34 +2,20 @@ package main
 
 import (
 	"html/template"
-	"log"
 	"net/http"
 )
 
-// Struct to send data to HTML
-type PageData struct {
-	Title   string
-	Message string
-}
-
 func main() {
-	// Route handler
 	http.HandleFunc("/", homeHandler)
 
-	log.Println("Server running at http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.ListenAndServe(":8080", nil)
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	// Parse the HTML file
-	tmpl := template.Must(template.ParseFiles("index.html"))
-
-	// Data to send to HTML
-	data := PageData{
-		Title:   "Go + HTML Connected",
-		Message: "This text is coming from Go!",
+	tmpl, err := template.ParseFiles("templates/main.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
-
-	// Inject data into HTML
-	tmpl.Execute(w, data)
+	tmpl.Execute(w, nil)
 }
